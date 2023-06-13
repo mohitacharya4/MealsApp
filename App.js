0be0d8fc34d359d4d86b160781589520d6574f2d
mohitacharya4 <mohitacharya4@gmail.com>
2023-06-13 18:31:14 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+
+export default function App() {
+  return <CategoriesScreen />;
+}
+
+const styles = StyleSheet.create({});
---
+++ components/CategoryGridTile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Pressable, Text } from "react-native";
+
+export default function CategoryGridTile({ title, color }) {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
